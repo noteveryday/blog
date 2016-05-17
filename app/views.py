0a9b7ebd254f0d844cf0c6401d2921ab74abb8d0
noteveryday <bzflyg@gmail.com>
2016-05-17 11:48:53 +0800

    add form

--- app/views.py
+++ app/views.py
@@ -2,14 +2,24 @@ from flask import render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 from app import app
-
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-@app.route('/')
-@app.route('/index')
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    user = {'nickname': 'Miguel'}
+    # user = {'nickname': 'Miguel'}
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
     posts = [
         {'author': {'nickname': "john"},
          'body': 'Beautiful day in Portland!'},
@@ -20,6 +30,7 @@ def index():
 
     return render_template('index.html',
                            title = 'Home',
-                           user = user,
+                           form = form,
+                           name = name,
                            posts = posts,
                            current_time=current_time) 
