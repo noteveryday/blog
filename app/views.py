3c1cd967500b8fa2ea63751bd8913f58f82d1324
noteveryday <bzflyg@gmail.com>
2016-05-17 14:33:04 +0800

    base.html

--- app/views.py
+++ app/views.py
@@ -1,25 +1,30 @@
-from flask import render_template
+from flask import render_template, session, redirect, url_for
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import Required, Email
 from app import app
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
+    email = StringField('What\'s your email', validators=[Email()])
     submit = SubmitField('Submit')
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     # user = {'nickname': 'Miguel'}
     name = None
+    email = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
+        # name = form.name.data
+        # email = form.email.data
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        return redirect(url_for('index'))
     posts = [
         {'author': {'nickname': "john"},
          'body': 'Beautiful day in Portland!'},
@@ -31,6 +36,7 @@ def index():
     return render_template('index.html',
                            title = 'Home',
                            form = form,
-                           name = name,
+                           name = session.get('name'),
+                           email = session.get('email'),
                            posts = posts,
                            current_time=current_time) 
